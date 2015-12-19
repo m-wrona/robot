6782878028fcb1011900bc1d1a99fae2345e020e
Michal Wronski <michal.robert.wronski@gmail.com>
2015-12-19 22:33:52 +0100

    First program run on activity board.

--- Learn/Examples/Circuits/Blink Light.c
+++ Learn/Examples/Circuits/Blink Light.c
@@ -13,8 +13,8 @@ int main()                                    // main function
   while(1)                                    // Endless loop
   {
     high(26);                                 // Set P26 I/O pin high
-    pause(100);                               // Wait 1/10 second
+    pause(1000);                               // Wait 1/10 second
     low(26);                                  // Set P26 I/O pin low
-    pause(100);                               // Wait another 1/10 second
+    pause(1000);                               // Wait another 1/10 second
   }
 }
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # robot
 Robot built based on [Propeller Activity Board](http://learn.parallax.com/activitybot)
+
+
+# Troubleshooting
+
+1) Add your user to dialout group in order to be able to push stuff to COM ports
+
+```
+sudo adduser MyUser dialout
+```
+
+2) Make sure that 'rw' rights are granted for COM ports
+
+```
+sudo chmod a+rw /dev/ttyUSB0
+```
